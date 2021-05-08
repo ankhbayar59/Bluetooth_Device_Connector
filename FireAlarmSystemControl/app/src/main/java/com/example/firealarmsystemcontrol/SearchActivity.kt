60ab86df57c9e03d53182e11a6d25eb490cabe43
Ankhbayar <ankhbayarmiller@yahoo.com>
2021-05-08 07:21:23 -0700

    Update 05/08/2021

--- FireAlarmSystemControl/app/src/main/java/com/example/firealarmsystemcontrol/SearchActivity.kt
+++ FireAlarmSystemControl/app/src/main/java/com/example/firealarmsystemcontrol/SearchActivity.kt
@@ -9,22 +9,21 @@ import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanFilter
 import android.bluetooth.le.ScanResult
 import android.bluetooth.le.ScanSettings
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.SimpleItemAnimator
 import android.content.Context
-
 import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.search.*
+import android.os.Handler
+import android.os.Looper
+import android.os.ParcelUuid
+import android.util.Log
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.search.*
 import org.jetbrains.anko.alert
 import java.util.*
-
 
 
 class SearchActivity : AppCompatActivity() {
@@ -34,7 +33,6 @@ class SearchActivity : AppCompatActivity() {
         return ContextCompat.checkSelfPermission(this, permissionType) ==
                 PackageManager.PERMISSION_GRANTED
     }
-
     private val bluetoothAdapter: BluetoothAdapter by lazy {
         val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
         bluetoothManager.adapter
@@ -42,216 +40,203 @@ class SearchActivity : AppCompatActivity() {
     private val bluetoothScanner by lazy {
         bluetoothAdapter.bluetoothLeScanner
     }
+
+    var byteString : String = "1"
+    var byteStringZero : String = "0"
+    var writeData = byteString.hexToBytes()
+
     // Used to stop or start bluetooth scan
-    private var scanState : Boolean = false;
     private val scanResults = mutableListOf<ScanResult>()
-    private var isFirstScan : Boolean = true;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.search)
         supportActionBar?.hide()
-        if(!grantedLocationPermission){
+        populateFilter()
+        if (!grantedLocationPermission) {
             enableLocation()
         }
-
         val stopScan = findViewById<Button>(R.id.stopScan)
         stopScan.setOnClickListener {
-            if(scanState) {
-                if(isFirstScan){
-                    runOnUiThread() {
-                        stopScan.setText("STOP")
-                    }
-                    isFirstScan = false
-                }
-                else{
-                    runOnUiThread {
-                        stopScan.setText("START")
-                    }
-                }
-                scanState = false
+            scanBLE()
+            Handler(Looper.getMainLooper()).postDelayed({
                 stopScanning()
-            }
-            else{
-                runOnUiThread {
-                    stopScan.setText("STOP")
+                scanResults.forEach() { result ->
+                    bleDeviceObject.connect(result.device, this@SearchActivity)
                 }
-                scanState = true
-                scanBLE()
+            }, 4000)
+        }
+        val allsilenceButton = findViewById<Button>(R.id.allsilence)
+        allsilenceButton.setOnClickListener {
+            val indexOfFirstAlarm = scanResults.indexOfFirst {
+                it.device.name == "fireAlarm1"
             }
-        }
-        //setting up our recycler view
-        initRecyclerViewForScanner()
+            /*
+            val indexOfSecondAlarm = scanResults.indexOfFirst {
+                it.device.name == "fireAlarm2"
+            }
+            */
 
+            val characteristicsOfFirstAlarm by lazy {
+                bleDeviceObject.servicesOnDevice(scanResults.get(indexOfFirstAlarm).device)?.flatMap { service ->
+                    service.characteristics ?: listOf()
+                } ?: listOf()
+            }
+            /*
+            val characteristicsOfSecondAlarm by lazy {
+                bleDeviceObject.servicesOnDevice(scanResults.get(indexOfSecondAlarm).device)?.flatMap { service ->
+                    service.characteristics ?: listOf()
+                } ?: listOf()
+            }
+             */
+            val firstSirenCharacteristicUUID = ParcelUuid.fromString("00010001-2345-2312-9231-40f6e305f9ee")
+            val firstSirenCharacteristicIndex = characteristicsOfFirstAlarm.indexOfFirst {
+                it.uuid == firstSirenCharacteristicUUID.uuid
+            }
+            /*
+            val secondSirenCharacteristicUUID = ParcelUuid.fromString("00010001-2345-2312-9231-40f6e305f9ff")
+            val secondSirenCharacteristicIndex = characteristicsOfSecondAlarm.indexOfFirst {
+                it.uuid == secondSirenCharacteristicUUID.uuid
+            }
+
+             */
+            writeData = byteStringZero.hexToBytes()
+            bleDeviceObject.writeCharacteristic(scanResults[indexOfFirstAlarm].device,
+                    characteristicsOfFirstAlarm[firstSirenCharacteristicIndex], writeData)
+            /*
+            bleDeviceObject.writeCharacteristic(scanResults[indexOfSecondAlarm].device,
+                    characteristicsOfFirstAlarm[secondSirenCharacteristicIndex], writeData)
+
+             */
+        }
+        val alltriggerButton = findViewById<Button>(R.id.alltrigger)
+        alltriggerButton.setOnClickListener {
+            val indexOfFirstAlarm = scanResults.indexOfFirst {
+                it.device.name == "fireAlarm1"
+            }
+            Log.i("indexing", "past indexing and value is $indexOfFirstAlarm")
+            /*
+            val indexOfSecondAlarm = scanResults.indexOfFirst {
+                it.device.name == "fireAlarm2"
+            }
+
+             */
+            val characteristicsOfFirstAlarm by lazy {
+                bleDeviceObject.servicesOnDevice(scanResults.get(indexOfFirstAlarm).device)?.flatMap { service ->
+                    service.characteristics ?: listOf()
+                } ?: listOf()
+            }
+            Log.i("characteristic", "past characteristics finding")
+            /*
+            val characteristicsOfSecondAlarm by lazy {
+                bleDeviceObject.servicesOnDevice(scanResults.get(indexOfSecondAlarm).device)?.flatMap { service ->
+                    service.characteristics ?: listOf()
+                } ?: listOf()
+            }
+
+             */
+            val firstSirenCharacteristicUUID = ParcelUuid.fromString("00010001-2345-2312-9231-40f6e305f9ee")
+            val firstSirenCharacteristicIndex = characteristicsOfFirstAlarm.indexOfFirst {
+                it.uuid == firstSirenCharacteristicUUID.uuid
+            }
+            Log.i("UUID", "Past UUID of characteristic")
+            /*
+            val secondSirenCharacteristicUUID = ParcelUuid.fromString("00010001-2345-2312-9231-40f6e305f9ff")
+            val secondSirenCharacteristicIndex = characteristicsOfSecondAlarm.indexOfFirst {
+                it.uuid == secondSirenCharacteristicUUID.uuid
+            }
+
+             */
+
+            writeData = byteString.hexToBytes()
+            bleDeviceObject.writeCharacteristic(scanResults[indexOfFirstAlarm].device,
+                    characteristicsOfFirstAlarm[firstSirenCharacteristicIndex], writeData)
+            Log.i("Write Trigger", "Past Write")
+            /*
+            bleDeviceObject.writeCharacteristic(scanResults[indexOfSecondAlarm].device,
+                    characteristicsOfFirstAlarm[secondSirenCharacteristicIndex], writeData)
+
+             */
+        }
     }
 
-    // in the event we leave this activity, bluetooth scan must be stopped.
-    //to reduce power consumption and not have the bluetooth scan running on the background
-    override fun onPause() {
-        super.onPause()
-        if(scanState){
-            scanState = false
-            stopScanning()
-        }
-    }
 
     override fun onResume() {
         super.onResume()
-        bleDeviceObject.registerListener(searchListener)
-    }
-
-    private val bleScanAdapter: ScannerAdapter by lazy {
-        ScannerAdapter(scanResults) { result ->
-
-            // with bluetooth low energy is recommended to stop scanning
-            // when trying to establish connection to bluetooth device
-
-            if(scanState){
-                stopScanning()
-                scanState = false
-            }
-
-            // Letting user know that bluetooth scan is being initiated
-
-            alert {
-                title = "Starting Connection to device"
-                positiveButton("OK") {}
-            }.show()
-
-            with(result.device){
-                bleDeviceObject.connect(this, this@SearchActivity)
-            }
-        }
-    }
-
-    private fun initRecyclerViewForScanner() {
-        recyclerViewForScanner.apply {
-            adapter = bleScanAdapter
-            layoutManager = LinearLayoutManager(
-                this@SearchActivity,
-                RecyclerView.VERTICAL,
-                false
-            )
-            isNestedScrollingEnabled = false
-        }
-
-        val searchResultAnimator = recyclerViewForScanner.itemAnimator
-        if (searchResultAnimator is SimpleItemAnimator) {
-            searchResultAnimator.supportsChangeAnimations = false
-        }
-    }
-
-    private val searchListener by lazy {
-        ConnectionListener().apply {
-            onConnection = { gatt ->
-                    Intent(this@SearchActivity, FirealarmoperationActivity::class.java).also {
-                    it.putExtra(BluetoothDevice.EXTRA_DEVICE, gatt.device)
-                    startActivity(it)
-                }
-                bleDeviceObject.unregisterListener(this)
-            }
-            onDisconnect = {
-                alert {
-                    title = "Disconnected"
-                    message = "Disconnected or unable to connect to device."
-                    positiveButton("OK") {}
-                }.show()
-            }
+        if (!bluetoothAdapter.isEnabled) {
+            enableBluetooth()
         }
     }
 
 
-
-
-    // filter is used to reduce amount of searching by only looking for bluetooth low energy devices
-    // with specific UUID that our bluetooth low energy fire alarm device is advertising
-    /*
-    val uuid = ParcelUuid( it would be the custom random Uuid)
-    private val filterForScanning = ScanFilter.Builder().setServiceUuid().build()
-    */
-
+    private val firstAlarm = ParcelUuid.fromString("00010000-89bd-43c8-9231-40f6e305f9bb")
+    private val secondAlarm = ParcelUuid.fromString("1bb00766-f72c-406c-8ca4-1b6fe066f6cc")
+    private val firstFilter = ScanFilter.Builder().setServiceUuid(firstAlarm).build()
+    private val secondFilter = ScanFilter.Builder().setServiceUuid(secondAlarm).build()
     //scanModeSettings will be used for parameter for
     // specifying the scan mode to be balanced to keep it sho
     // to find our bluetooth low energy fire alarm device
     private val scanModeSettings = ScanSettings.Builder()
         .setScanMode(ScanSettings.SCAN_MODE_BALANCED)
         .build()
-
+    private var filters = mutableListOf<ScanFilter>()
+    private fun populateFilter() {
+        filters.add(firstFilter)
+        filters.add(secondFilter)
+    }
     private fun scanBLE() {
-        // before every scan, the populated list of results from previous should be cleared
-        scanResults.clear()
-        bleScanAdapter.notifyDataSetChanged()
-        bluetoothScanner.startScan(null, scanModeSettings, scanCallBack)
+        Log.i("scanBLE", "before bluetooth scan begins")
+        bluetoothScanner.startScan(filters, scanModeSettings, scanCallBack)
     }
 
-    private fun stopScanning() {
 
+
+
+    private fun stopScanning() {
         // simple and straightforward, telling the app platform device's bluetooth adapter to stop scanning
         // as mentioned before, it is safer and saves energy to stop bluetooth scan if not needed.
         bluetoothScanner.stopScan(scanCallBack)
     }
-
     private val scanCallBack = object : ScanCallback() {
         override fun onScanResult(typeOfCallBack: Int, resultFromScan: ScanResult) {
-            // scanResults.indexOfFirst returns of index of the first item that is located in the list,
-            // we are using the mac address to find
-            // we seeing if device we scanned already exist in the list
-            // if it doesn't -1 is returned
-            val isExistIndex = scanResults.indexOfFirst{
-                it.device.address == resultFromScan.device.address
-            }
-
-            // we just wanna check if the isExist is anything other than -1,
-            // but it should be between 0 to the size of the list
-
-            if (isExistIndex == -1) {
-                // this device doesn't exist in our scanned list
-                scanResults.add(resultFromScan)
-                // we are notifying our recycler view for our scanned results
-                // that we have made insertion into the list
-                bleScanAdapter.notifyItemInserted(scanResults.size - 1)
-            } else {
-                // if the device is already in the list, then we just insert it
-                scanResults[isExistIndex] = resultFromScan
-
-                // letting the Recycler know itemChanged
-                // the reason we still add the device it's existing index is
-                // the list needs to be up to date for better user experience
-                // we can skip, but the
-                bleScanAdapter.notifyItemChanged(isExistIndex)
-            }
+            scanResults.add(resultFromScan)
         }
     }
 
     private fun enableLocation () {
         // sending alert to user about why location permission must be granted
-        runOnUiThread {
-            alert {
-                title = "Location Permission Required!"
-                message = "The system requires the app to be granted location " +
-                        "permission in order to scan for bluetooth low energy devices"
-                positiveButton("OK") {
-                    requestPermission(
+        alert {
+            title = "Location Permission Required!"
+            message = "The system requires the app to be granted location " +
+                    "permission in order to scan for bluetooth low energy devices"
+            positiveButton("OK") {
+                requestPermission(
                         Manifest.permission.ACCESS_FINE_LOCATION,
                         1
-                    )
-                }
+                )
+            }
+            }.show()
+    }
+
+    private fun enableBluetooth() {
+        if (!bluetoothAdapter.isEnabled) {
+            val intentBluetooth = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            // Alerting the user to enable bluetooth on their device
+            // in order to scan for bluetooth low energy devices
+            alert {
+                title = "Bluetooth Must Be Enabled!"
+                message = "In order to scan for bluetooth low energy devices, " +
+                        "bluetooth must be enabled on the user's device"
+                positiveButton("OK") {startActivityForResult(intentBluetooth, 1)}
             }.show()
         }
-        /*
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("Location Permission Required")
-        builder.setMessage("The system requires the app to be granted location"
-                + "permission in order to scan for bluetooth low energy devices")
-        builder.setPositiveButton("Understood") { dialogInterface: DialogInterface, i: Int ->
-            requestPermission(Manifest.permission.ACCESS_FINE_LOCATION, 1)
-        }
-        builder.show()
-
-         */
     }
 
     private fun Activity.requestPermission(permission: String, requestCode: Int) {
         ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
     }
 
+    private fun String.hexToBytes() =
+            this.chunked(2).map { it.toUpperCase(Locale.US).toInt(16).toByte() }.toByteArray()
 }
+
+
--- FireAlarmSystemControl/app/src/main/java/com/example/firealarmsystemcontrol/bleDeviceObject.kt
+++ FireAlarmSystemControl/app/src/main/java/com/example/firealarmsystemcontrol/bleDeviceObject.kt
@@ -21,7 +21,6 @@ import java.util.concurrent.ConcurrentLinkedQueue
 
 object bleDeviceObject {
 
-    private var bleListener : MutableSet<WeakReference<ConnectionListener>> = mutableSetOf()
     private val bleDeviceGattMap = ConcurrentHashMap<BluetoothDevice, BluetoothGatt>()
     private val bleQueue = ConcurrentLinkedQueue<bleDeviceData>()
     private var onWait: bleDeviceData? = null
@@ -29,25 +28,10 @@ object bleDeviceObject {
     fun servicesOnDevice(device: BluetoothDevice): List<BluetoothGattService>? =
         bleDeviceGattMap[device]?.services
 
-    fun registerListener(listener: ConnectionListener) {
-        if (bleListener.map { it.get() }.contains(listener)) { return }
-        bleListener.add(WeakReference(listener))
-        bleListener = bleListener.filter { it.get() != null }.toMutableSet()
-    }
 
-    fun unregisterListener(listener: ConnectionListener) {
-        var unregister: WeakReference<ConnectionListener>? = null
-        bleListener.forEach {
-            if (it.get() == listener) {
-                unregister = it
-            }
-        }
-        unregister?.let {
-            bleListener.remove(it)
-        }
-    }
 
     fun connect(device: BluetoothDevice, context: Context) {
+        Log.i("connect", "inConnect")
         if (device.isConnected()) {
         } else {
             addQueue(Connect(device, context.applicationContext))
@@ -58,17 +42,12 @@ object bleDeviceObject {
     fun closeConnection(device: BluetoothDevice) {
         if (device.isConnected()) {
             addQueue(Disconnect(device))
-        } else {
         }
     }
 
     fun readCharacteristic(device: BluetoothDevice, characteristic: BluetoothGattCharacteristic) {
-        if (device.isConnected() && characteristic.allowsRead()) {
+        if (device.isConnected()) {
             addQueue(readingDataFromCharacteristic(device, characteristic.uuid))
-        } else if (!(characteristic.allowsRead())) {
-            Log.i("readCharacteristic", "character uuid ${characteristic.uuid} that isn't readable!")
-        } else if (!device.isConnected()) {
-            Log.i("readCharacteristic","Not connected to ${device.address}, cannot perform characteristic read")
         }
     }
 
@@ -78,17 +57,9 @@ object bleDeviceObject {
         characteristic: BluetoothGattCharacteristic,
         writeData: ByteArray
     ) {
-        val writeType = when {
-            characteristic.allowsWrite() -> BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT
-            else -> {
-                Log.i("writeCharacteristic","Characteristic ${characteristic.uuid} cannot be written to")
-                return
-            }
-        }
+        val writeType = BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT
         if (device.isConnected()) {
             addQueue(writingDataToCharacteristic(device, characteristic.uuid, writeType, writeData))
-        } else {
-            Log.i("writeCharacteristic","Not connected to ${device.address}, cannot perform characteristic write")
         }
     }
 
@@ -103,7 +74,6 @@ object bleDeviceObject {
 
 
     private fun end() {
-        Log.i("signEndOfOperation","End of $onWait")
         onWait = null
         if (bleQueue.isNotEmpty()) {
             start()
@@ -112,20 +82,17 @@ object bleDeviceObject {
 
     private fun start() {
         if (onWait != null) {
-            Log.i("doNextOepration","doNextOperation() called when an operation is pending! Aborting.")
             return
         }
 
 
         val nextOperation = bleQueue.poll() ?: run {
-            Log.i("doNextOperation","Operation queue empty, returning")
             return
         }
         onWait = nextOperation
 
         if (nextOperation is Connect) {
             with(nextOperation) {
-                Log.i("doNextOperation","Connecting to ${device.address}")
                 device.connectGatt(context, false, callback)
             }
             return
@@ -134,18 +101,10 @@ object bleDeviceObject {
 
         val gatt = bleDeviceGattMap[nextOperation.device]
             ?: this@bleDeviceObject.run {
-                Log.i("doNextOperation","Not connected to ${nextOperation.device.address}! Aborting $nextOperation operation.")
                 end()
                 return
             }
         when (nextOperation) {
-            is Disconnect -> with(nextOperation) {
-                //Timber.w("Disconnecting from ${device.address}")
-                gatt.close()
-                bleDeviceGattMap.remove(device)
-                bleListener.forEach { it.get()?.onDisconnect?.invoke(device) }
-                end()
-            }
             is writingDataToCharacteristic -> with(nextOperation) {
                 gatt.findCharacteristic(UuidOfCharacteristic)?.let { characteristic ->
                     characteristic.writeType = writeType
@@ -167,21 +126,16 @@ object bleDeviceObject {
 
     private val callback = object : BluetoothGattCallback() {
         override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
-            val deviceAddress = gatt.device.address
-
             if (status == BluetoothGatt.GATT_SUCCESS) {
                 if (newState == BluetoothProfile.STATE_CONNECTED) {
-                    Log.i("BluetoothGattCallback","onConnectionStateChange: connected to $deviceAddress")
                     bleDeviceGattMap[gatt.device] = gatt
                     Handler(Looper.getMainLooper()).post {
                         gatt.discoverServices()
                     }
                 } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
-                    Log.i("BluetoothGattCallback","onConnectionStateChange: disconnected from $deviceAddress")
                     closeConnection(gatt.device)
                 }
             } else {
-                Log.i("BluetoothGattCallback","onConnectionStateChange: status $status encountered for $deviceAddress!")
                 if (onWait is Connect) {
                     end()
                 }
@@ -190,16 +144,6 @@ object bleDeviceObject {
         }
 
         override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
-            with(gatt) {
-                if (status == BluetoothGatt.GATT_SUCCESS) {
-                    Log.i("onServiceDiscovered","Discovered ${services.size} services for ${device.address}.")
-                    bleListener.forEach { it.get()?.onConnection?.invoke(this) }
-                } else {
-                    Log.i("onServiceDiscovered","Service discovery failed due to status $status")
-                    closeConnection(gatt.device)
-                }
-            }
-
             if (onWait is Connect) {
                 end()
             }
@@ -210,21 +154,6 @@ object bleDeviceObject {
             characteristic: BluetoothGattCharacteristic,
             status: Int
         ) {
-            with(characteristic) {
-                when (status) {
-                    BluetoothGatt.GATT_SUCCESS -> {
-                        Log.i("onCharacteristicRead","Read characteristic $uuid | value: ${value}")
-                        bleListener.forEach { it.get()?.onCharacteristicRead?.invoke(gatt.device, this) }
-                    }
-                    BluetoothGatt.GATT_READ_NOT_PERMITTED -> {
-                        Log.i("onCharacteristicRead","Read not permitted for $uuid!")
-                    }
-                    else -> {
-                        Log.i("onCharacteristicRead","Characteristic read failed for $uuid, error: $status")
-                    }
-                }
-            }
-
             if (onWait is readingDataFromCharacteristic) {
                 end()
             }
@@ -235,24 +164,20 @@ object bleDeviceObject {
             characteristic: BluetoothGattCharacteristic,
             status: Int
         ) {
-            with(characteristic) {
-                when (status) {
-                    BluetoothGatt.GATT_SUCCESS -> {
-                        Log.i("onCharacteristicWrite","Wrote to characteristic $uuid | value: ${value}")
-                        bleListener.forEach { it.get()?.onCharacteristicWrite?.invoke(gatt.device, this) }
-                    }
-                    BluetoothGatt.GATT_WRITE_NOT_PERMITTED -> {
-                        Log.i("onCharacteristicWrite","Write not permitted for $uuid!")
-                    }
-                    else -> {
-                        Log.i("onCharacteristicWrite","Characteristic write failed for $uuid, error: $status")
-                    }
-                }
-            }
-
             if (onWait is writingDataToCharacteristic) {
                 end()
             }
         }
     }
+    fun BluetoothGatt.findCharacteristic(uuid: UUID): BluetoothGattCharacteristic? {
+        services?.forEach { service ->
+            service.characteristics?.firstOrNull { characteristic ->
+                characteristic.uuid == uuid
+            }?.let { matchingCharacteristic ->
+                return matchingCharacteristic
+            }
+        }
+        return null
+    }
+
 }
